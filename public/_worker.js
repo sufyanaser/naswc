8060ee096d13972af3358819b17ad77902095c6d
‪sufian nasser‬‏ <166199800+sufyanaser@users.noreply.github.com>
2026-06-30 17:40:18 +0300

    Inject Team Orbit stylesheet on Cloudflare Pages

---
+++ public/_worker.js
@@ -0,0 +1,17 @@
+export default {
+  async fetch(request, env) {
+    const response = await env.ASSETS.fetch(request);
+    const type = response.headers.get('content-type') || '';
+    if (!type.includes('text/html') || response.status !== 200) {
+      return response;
+    }
+
+    return new HTMLRewriter()
+      .on('head', {
+        element(element) {
+          element.append('\n<link rel="stylesheet" href="/team-orbit.css" data-nas-team-orbit="true">', { html: true });
+        }
+      })
+      .transform(response);
+  }
+};

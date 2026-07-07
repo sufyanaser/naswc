a592a662475514d0bab2014ad51c05fcc833345b
‪sufian nasser‬‏ <166199800+sufyanaser@users.noreply.github.com>
2026-07-07 14:33:50 +0300

    Load founder copy fix

--- public/render.js
+++ public/render.js
@@ -1,4 +1,1 @@
-import('/render-core.js')
-  .then(function(){return import('/custom-blocks-render.js')})
-  .then(function(){return import('/slideshow-polish.js')})
-  .catch(function(error){console.error(error)});
+import('/render-core.js').then(function(){return import('/custom-blocks-render.js')}).then(function(){return import('/slideshow-polish.js')}).then(function(){return import('/founder-copy-fix.js')}).catch(function(error){console.error(error)});

12275df34c9fe4db8367c41ca4d1196ca5eb9c0e
‪sufian nasser‬‏ <166199800+sufyanaser@users.noreply.github.com>
2026-07-07 19:33:15 +0300

    Refresh render module cache keys

--- public/render.js
+++ public/render.js
@@ -1,1 +1,6 @@
-import('/render-core.js').then(function(){return import('/partners-render.js')}).then(function(){return import('/custom-blocks-render.js')}).then(function(){return import('/slideshow-polish.js')}).then(function(){return import('/founder-copy-fix.js')}).then(function(){return import('/header-nav-polish.js')}).catch(function(error){console.error(error)});
+import '/render-core.js';
+import '/partners-render.js?v=orb1';
+import '/custom-blocks-render.js';
+import '/slideshow-polish.js';
+import '/founder-copy-fix.js';
+import '/header-nav-polish.js?v=partners1';
